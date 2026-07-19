1431408febf60d9fc02559fbe5e4029a858e2def
sunilkumardevarapalli <kumar.36021@gmail.com>
2026-07-19 08:12:42 +0530

    Delete EventLedger directory

--- EventLedger/account-service/src/main/java/com/eventledger/accountservice/domain/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.eventledger.accountservice.domain;
-
-public enum TransactionType {
-    CREDIT, DEBIT
-}
-
--- EventLedger/event-gateway/src/main/java/com/eventledger/eventgateway/domain/EventStatus.java
+++
@@ -1,6 +0,0 @@
-package com.eventledger.eventgateway.domain;
-
-public enum EventStatus {
-    PENDING, PROCESSED, FAILED
-}
-
